d865858db9b173db6e8ae995cb8a4bf1746ce3bb
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-04-22 12:48:04 +0900

    fix xyz for 1.9

--- hires_fix_tweaks/xyz.py
+++ hires_fix_tweaks/xyz.py
@@ -3,7 +3,7 @@ from modules import scripts
 
 def xyz_grid_axis():
     for data in scripts.scripts_data:
-        if data.script_class.__module__ == 'xyz_grid.py' and hasattr(data, 'module'):
+        if data.script_class.__module__ in ('scripts.xyz_grid', 'xyz_grid.py') and hasattr(data, 'module'):
             xyz_grid = data.module
             xyz_grid.axis_options.extend(
                 [
@@ -11,3 +11,5 @@ def xyz_grid_axis():
                 ]
             )
             break
+    else:
+        raise ImportError("Can't find scripts.xyz_grid: Hires CFG Scale won't work in XYZ Grid")
