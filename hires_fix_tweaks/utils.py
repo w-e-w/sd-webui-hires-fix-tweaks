1cb2f56c382a9bb28f73adb5c6ad1b362fc5dad9
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-09-14 23:54:37 +0900

    json.dumps ensure_ascii=False

--- hires_fix_tweaks/utils.py
+++ hires_fix_tweaks/utils.py
@@ -4,7 +4,7 @@ quote_swap = str.maketrans('\'"', '"\'')
 
 
 def dumps_quote_swap_json(input_object):
-    return json.dumps(input_object).translate(quote_swap)
+    return json.dumps(input_object, ensure_ascii=False).translate(quote_swap)
 
 
 def loads_quote_swap_json(input_string):
